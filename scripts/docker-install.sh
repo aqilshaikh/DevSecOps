3746c5a8b2d56d66d37843ffe4c94d2ea2ca5c07
Aqil Shaikh <aqilshaikh94@gmail.com>
2021-08-09 18:27:26 +0530

    Update docker-install.sh
    update docker install

--- scripts/docker-install.sh
+++ scripts/docker-install.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 sudo apt update -y
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 
 sudo apt update -y
-sudo apt install docker-ce -y
+sudo apt install -y docker-ce 
 sudo systemctl enable docker
